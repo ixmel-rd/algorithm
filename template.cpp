8f00497b49f6de70f886aae97738e3adc8831629
ixmel <kazuyakazuya524@gmail.com>
2018-05-16 18:24:58 +0900

    nya-

--- template.cpp
+++ template.cpp
@@ -5,8 +5,10 @@
 #include<algorithm>	
 #include<map>
 #include<set>
+#include<unordered_map>
 #include<utility>
 #include<cmath>
+#include<random>
 #include<cstring>
 #include<queue>
 #include<stack>
@@ -18,6 +20,7 @@
 #include<typeinfo>
 #define loop(i,a,b) for(int i=a;i<b;i++) 
 #define rep(i,a) loop(i,0,a)
+#define FOR(i,a) for(auto i:a)
 #define pb push_back
 #define all(in) in.begin(),in.end()
 #define shosu(x) fixed<<setprecision(x)
@@ -37,20 +40,24 @@ typedef vector<vd> vvd;
 typedef pair<Def,pii> pip;
 typedef vector<pip>vip;
 //#define mt make_tuple
-//typedef tuple<pii,int,int> tp;
+//typedef tuple<int,int,int> tp;
 //typedef vector<tp> vt;
 template<typename A,typename B>bool cmin(A &a,const B &b){return a>b?(a=b,true):false;}
 template<typename A,typename B>bool cmax(A &a,const B &b){return a<b?(a=b,true):false;}
 //template<class C>constexpr int size(const C &c){return (int)c.size();}
 //template<class T,size_t N> constexpr int size(const T (&xs)[N])noexcept{return (int)N;}
 const double PI=acos(-1);
-const double EPS=1e-7;
-Def inf = sizeof(Def) == sizeof(long long) ? 2e18 : 1e9;
+const double EPS=1e-9;
+Def inf = sizeof(Def) == sizeof(long long) ? 2e18 : 1e9+10;
 int dx[]={0,1,0,-1};
 int dy[]={1,0,-1,0};
 int main(){
 	
 }
+
+
+
+
 
 
 
---
+++ template_right.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>	
+#define loop(i,a,b) for(int i=a;i<b;i++) 
+#define rep(i,a) loop(i,0,a)
+#define pb push_back
+#define all(in) in.begin(),in.end()
+using namespace std;
+typedef long long ll;
+typedef int Def;
+typedef pair<Def,Def> pii;
+typedef vector<Def> vi;
+Def inf = sizeof(Def) == sizeof(long long) ? 2e18 : 1e9+10;
+int main(){
+	
+}
+
+
+
+
+
+
+
+
