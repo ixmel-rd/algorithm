e36e37cd2cd777f779c1372c418b7faed9cb496a
ixmel <kazuyakazuya524@gmail.com>
2018-05-23 18:07:49 +0900

    templateを3分割

---
+++ template_med.cpp
@@ -0,0 +1,36 @@
+﻿//#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>	
+#include<map>
+#include<set>
+#include<cstring>
+#include<queue>
+#include<stack>
+#include<cstdio>
+#include<iomanip>
+//#define loop(i,a,b) for(int i=a;i<b;i++) 
+//#define rep(i,a) loop(i,0,a)
+#define pb push_back
+#define all(in) in.begin(),in.end()
+#define shosu(x) fixed<<setprecision(x)
+//using namespace std;
+typedef long long ll;
+typedef int Def;
+typedef pair<Def,Def> pii;
+typedef vector<Def> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vp;
+typedef vector<string> vs;
+Def inf = sizeof(Def) == sizeof(long long) ? 2e18 : 1e9+10;
+int main(){
+	
+}
+
+
+
+
+
+
+
+
