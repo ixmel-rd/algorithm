4785d9a31912bfa31d5e07c8c7b9afb5b8b64b21
ixmel <kazuyakazuya524@gmail.com>
2019-01-11 22:23:34 +0900

    MP修正

--- Data_stracture/MP.cpp
+++ Data_stracture/MP.cpp
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 //max common lenght prefix of s[0:i] and suffix of s[0:i]
 //aabaabaaa
 //_010123452
 // syuki of s[0:i] is i-mp[i]
 //abababcaa
 //122222778
-=======
->>>>>>> 8e1bf45f589c2063104bc26adb8cfe6ebddfea89
 struct MP{
 	vi a;
 	MP(){}
@@ -14,11 +11,3 @@ struct MP{
 		for(int i=0,j=-1;i<s.size();a[++i]=++j)while(j>=0&&s[i]!=s[j])j=a[j];
 	}
 };
-<<<<<<< HEAD
-int main(){
-	string s;
-	cin>>s;
-	MP mp(s);
-}
-=======
->>>>>>> 8e1bf45f589c2063104bc26adb8cfe6ebddfea89
