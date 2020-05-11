4775033c5bc4458bb732daf395585932e65b42dc
ixmelrd <is0255ie@ed.ritsumei.ac.jp>
2020-05-11 16:00:55 +0900

    Update eulerPhi.cpp

--- math/eulerPhi.cpp
+++ math/eulerPhi.cpp
@@ -1,4 +1,7 @@
 //oira- phi kansu
+// nと互いに素なi(1<=i<=n)の個数
+// O(sqrt(n))
+// n=12 -> 1,5,7,11 (out=4)
 int eulerPhi(int n){
 	int out=n;
 	for(int i=2;i*i<=n;i++){
@@ -9,9 +12,4 @@ int eulerPhi(int n){
 	}
 	if(n>1)out-=out/n;
 	return out;
-}
-int main(){
-	int n;
-	cin>>n;
-	cout<<eulerPhi(n)<<endl;
 }
