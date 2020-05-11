a12923b907714e8cd2dedef0437cd77f79d621df
ixmelrd <is0255ie@ed.ritsumei.ac.jp>
2020-05-11 15:56:28 +0900

    Update nCr.cpp

--- math/nCr.cpp
+++ math/nCr.cpp
@@ -1,15 +1,8 @@
-//nCr mod m
-#define MOD 1000000007
-#define M 1000000
-vector<ll> fact;
-bool h=false;
-void init(){
-	h=true;
-	fact=vector<ll>(M);
-	fact[0]=fact[1]=1;
-	loop(i,2,M)fact[i]=fact[i-1]*i%MOD;
-}
-// a^b mod MOD
+// const int MOD=1000000007;
+const int MOD=998244353;
+const int M=300000;
+vi fact,rev;
+bool do_init=false;
 ll powmod(ll a,ll b,ll m=MOD){
 	ll out=1;
 	ll p=a%m;
@@ -20,11 +13,19 @@ ll powmod(ll a,ll b,ll m=MOD){
 	}
 	return out;
 }
+void init(){
+	do_init=true;
+	fact=rev=vector<ll>(M);
+	fact[0]=fact[1]=1;
+	loop(i,2,M)fact[i]=fact[i-1]*i%MOD;
+	rev[M-1]=powmod(fact[M-1],MOD-2,MOD);
+	for(int i=M-2;i>=0;i--)rev[i]=rev[i+1]*(i+1)%MOD;
+}
 //nCr
 ll nCr(ll n,ll r,ll m=MOD){
-	if(!h)init();
-	if(n<0||r<0||n<r)return 1;//??????
-	ll out=fact[n]*powmod(fact[r]*fact[n-r]%m,m-2,m)%m;
+	if(!do_init)init();
+	if(n<0||r<0||n<r)return 1; // 0 or 1
+	ll out=fact[n]*rev[r]%m*rev[n-r]%m;;
 	return out;
 }
 //nHr
